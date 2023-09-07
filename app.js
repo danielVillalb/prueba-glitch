2a08c7deb9406f20b72478e5de6db1e12fa38f91
danielVillalb <villalbacarlosd97@gmail.com>
2023-09-06 21:32:26 -0300

    stilando movile first

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ let express=require('express')
 let path=require('path')
 let app=express();
 let port=3000
+app.use(express.json())
 app.listen(port,()=>{
     console.log('server andando')
 })
 app.get(('/'),(req,res)=>{
-    res.send('hola mundo')
+    res.send('./index.html')
+})
+
+
+app.post('/user',(req,res)=>{
+    const {user,password}=req.body
+    console.log('datos cargados correctamente',user,password)
+
+    res.send('este es el nombre de usuario y contrasenia'+user+password)
 })
